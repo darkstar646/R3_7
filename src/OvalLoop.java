652802f94e4195976b5212af611ddab5e736f0bb
user1 <user1@10.80.172.227>
2025-10-16 15:55:29 +0900

    2

--- src/OvalLoop.java
+++ src/OvalLoop.java
@@ -26,17 +26,79 @@ public class OvalLoop{
 }
 */
 
-
+/*
 public class OvalLoop extends MyFrame2{
 	public void run() {
+		int x=120;
+		int y=130;
+		int r_1=150;
+		int r_1_extra=(r_1-r_1/5);
+		int r_2=30;
+		int n=37025447%5+6;
+		int s=360/n;
+		//int first_loc_x=130+r_1;
+		//int first_loc_y=130+r_1/2-r_2/2;
+		System.out.println(""+n);
+		System.out.println(""+s);
+		
 		setColor(0,136,51);
-		fillOval(100,100,130,130);
+		//fillOval(130,130,r_1,r_1);
+		fillOval(r_1_extra,r_1_extra,r_1,r_1);
+		
+		for(int i=1; i<n+1; i++){
+			double cos=Math.cos(Math.toRadians(i*s));
+			double sin=Math.sin(Math.toRadians(i*s));
+			double loc_x=cos*(r_1/2+r_2/2)+x+(r_1/2-r_2/2);
+			double loc_y=sin*(r_1/2+r_2/2)+x+(r_1/2-r_2/2);
+			
+			fillOval(loc_x,loc_y,r_2,r_2);
+			System.out.println(""+cos);
+		}
+		
 	}
 
 	public static void main(String[] args) {
+		System.out.println("プログラムを実行します");
 		MyFrame2 frame1=new OvalLoop();
-		frame1.setLocation(200,200);
-		frame1.setSize(300,300);
 	}
 	
 }
+*/
+
+public class OvalLoop extends MyFrame2{
+	public void run() {
+		int x=160; 		//円の位置の左右
+		int y=160; 		//円の位置の上下		
+		int r_1=60; 		//大きい円の半径
+		int r_2=15;			//小さい円の半径		
+		int n=37025447%5+6;
+		int s=360/n;
+		//int x_1=(right+r_1/2); 		//大きい円の中心のx座標
+		//int y_1=(down+r_1/2); 		//大きい円の中心のy座標
+		
+		System.out.println(""+n);
+		System.out.println(""+s);
+		
+		setColor(0,136,51);
+		fillOval(x,y,r_1*2,r_1*2);
+		
+		for(int i=1; i<n+1; i++){
+			double cos=Math.cos(Math.toRadians(i*s));
+			double sin=Math.sin(Math.toRadians(i*s));
+			double loc_x=cos*(r_1+r_2)+x+(r_1-r_2);
+			double loc_y=sin*(r_1+r_2)+y+(r_1-r_2);
+			
+			fillOval(loc_x,loc_y,r_2*2,r_2*2);
+			System.out.println(""+cos);
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		System.out.println("プログラムを実行します");
+		MyFrame2 frame1=new OvalLoop();
+		frame1.setSize(500,500);
+	}
+	
+}
+
